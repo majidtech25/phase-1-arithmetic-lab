ce0b3685f73f1335594358dcfaa377ce330160a7
majidtech25 <abdulshide87@gmail.com>
2025-03-03 00:16:39 +0300

    Assignment- arthmetic-done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 18;
+let num4 = 4;
+let mod = Math.floor(num3 / num4);
+
+let max = Math.max(1, 2, 3, 4, 20);
